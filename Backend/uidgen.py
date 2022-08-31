64c4386968287c170c3ec15b39850e27ae5fc700
Raihan Khan <iamrk98@gmail.com>
2022-09-01 01:32:16 +0530

    created uid generation function

---
+++ Backend/uidgen.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import random
+
+def genrateuid():
+    now = str(datetime.now())
+    now = now.replace("-", "")
+    now = now.replace(":", "")
+    now = now.replace(" ", "")
+    uid = now[0:12]
+
+    return uid
+
+genrateuid()
