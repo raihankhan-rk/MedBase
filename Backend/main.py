716fc352f2ac1731c853a052e648ac29c6e74a4f
Raihan Khan <iamrk98@gmail.com>
2022-08-31 09:49:10 +0530

    Created 2 separate folders for Backend and Frontend all in the same repo

--- Backend/main.py
+++ Backend/main.py
@@ -3,8 +3,8 @@ from database import *
 db = Database.initializedatabase()
 
 collection = db.collection('programmer_details')
-res = collection.document('A01').set({  # insert document
-    'name': 'Vishnu',
+res = collection.document('8961675822').set({  # insert document
+    'name': 'Raihan',
     'age': 19,
     'Country': 'India',
     'Programming_languages': ['Python', 'C#', 'C++']
